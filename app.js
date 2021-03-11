88af11e270645cb7f09d47e7a1b6a34ff844c28c
LisaPiano <65650875+LisaPiano@users.noreply.github.com>
2021-03-10 23:43:15 -0700

    Update app.js
    Needs transition and polishing

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+//run function playGame
+function playGame(){
+  console.log('hi!');
+
+//declare universal variables
 const qwerty = document.getElementById('qwerty');
 let resetButton = document.getElementsByClassName('btn__reset')[0];
 const overlay = document.getElementById('overlay');
 let ul = document.getElementById('phrase');
 let button = document.querySelector('button');// variable for the keys on the qwerty keyboard
-let newPhrase; 
+let timesLost = 0;// number of incorrect guesses; increments
+
+//event listener for reset button that initially removes the overaly 
+resetButton.addEventListener('click', () => {
+  overlay.style.display = 'none';
+});
 
  //create new button that says RESET
  let newButton = document.createElement('button');//create new button that says RESET
@@ -13,12 +23,6 @@ let newPhrase;
  div.appendChild(newButton);   
  newButton.style.display = 'none';
  
-   
-//event listener for reset button that initially removes the overaly 
-resetButton.addEventListener('click', () => {
-    overlay.style.display = 'none';
-});
-
 //array of phrases to be used in the game
 const phrases = [
   "To be or not to be",
@@ -37,9 +41,10 @@ const getRandomPhraseAsArray = (array) => {
   return(array[num]);
 };
 let array = getRandomPhraseAsArray(phrases);
+console.log(array); 
 
 //Function that takes any array and returns it as a string of chars
- const addPhraseToDisplay = (array) => {
+ let addPhraseToDisplay = (array) => {
   for (let i = 0; i < array.length; i++){
   let li = document.createElement('li');
       li.textContent = array.charAt(i);
@@ -48,24 +53,30 @@ let array = getRandomPhraseAsArray(phrases);
             li.className += "space";
         } else {
             li.className += "letter";
+            li.style.backgroundColor = "purple";
         };
       ul.appendChild(li);
   }//end for  
  };//end function
  addPhraseToDisplay(array);
 
- const phraseLetters = document.querySelectorAll('li');//variable for the letters in the array phrase
- let timesLost = 0;// number of incorrect guesses; increments
-
  //Function that checks to see if a specific letter is in the selected phrase  
-const checkLetter = (button) => {
+let checkLetter = (button) => {
   let isMatch = null;
+  const phraseLetters = document.querySelectorAll('li');//variable for all the letters in the array phrase
   for (i=0; i < phraseLetters.length; i++) {
     if (button === phraseLetters[i].textContent.toLowerCase()) {
+      console.log(button);
+      console.log(phraseLetters);
+      console.log('phraseLetters length is: ' + phraseLetters.length);
       phraseLetters[i].classList.add('show');
+      phraseLetters[i].style.color = 'yellow';
+      
+      console.log('yes!');
       isMatch = true;
     } //end if
   }//end for
+
   return isMatch;
 };//end checkLetter
 
@@ -82,9 +93,6 @@ qwerty.addEventListener('click', event => {
       heart[heartNumber].setAttribute('src', "images/lostHeart.png");
     };//end second if 
   }; //end first if
-
-    
-
 
     //function to check if the player has won or lost---every time a letter is chosen
     const checkWin = () => {
@@ -121,45 +129,73 @@ qwerty.addEventListener('click', event => {
      
        
       //When reset button is clicked, 
-     newButton.addEventListener('click', () => {
+      newButton.addEventListener('click', () => {
       overlay.style.display = 'none';//remove overlay-works
       timesLost = 0; //resetting timesLost to 0--works
       heartNumber = 5-timesLost; //resetting heartNumber to 5--works
       
-  
         //reset live-heart images
         const heart = document.querySelectorAll('img');
         for(let i =0; i < 5; i++){
         heart[i].setAttribute('src', "images/liveHeart.png");
         };//end for
         
-                   //reset key classes to '' empty string
-                   let keys = document.getElementsByTagName('BUTTON');
-                   for(let i = 0; i <= 26; i++){
-                     if(keys[i].className === 'chosen'){
-                       keys[i].className = ' ';
-                       keys[i].disabled = false; //THIS WORKS!!!!
-                     }//end if
-                   };//end for
+        //reset key classes to '' empty string
+        let keys = document.getElementsByTagName('BUTTON');
+        for(let i = 0; i <= 26; i++){
+          if(keys[i].className === 'chosen'){
+            keys[i].className = ' ';
+            keys[i].disabled = false; //THIS WORKS!!!!
+          }//end if
+        };//end for
 
-                   //remove phrase
-                   let main = document.querySelector('.main-container');
-                    main.removeChild(phrase);
+        //remove phrase
+        let main = document.querySelector('.main-container');
+        main.removeChild(phrase);
 
-                 //create a new phrase div section
-                 let resetPhrase = document.createElement('div');
-                
-                 resetPhrase.setAttribute('id', 'phrase');
-                 resetPhrase.className = "section"; 
-                 main.insertBefore(resetPhrase, qwerty);
-              
+        //create a new phrase div section with UL added
+        let resetPhrase = document.createElement('div');
+        resetPhrase.setAttribute('id', 'phrase');
+        resetPhrase.className = "section"; 
+        main.insertBefore(resetPhrase, qwerty);
+        let ul = document.createElement('ul');
+        resetPhrase.appendChild(ul);
+        
+        
+        //generate a new phrase
+        let newArray = getRandomPhraseAsArray(phrases);
+        console.log(newArray);
+      
+        //add new phrase to the resetPhrase Div
+        //addPhraseToDisplay(newArray);---WHY WILL THIS NOT WORK????
+        
 
-          
+        for (let i = 0; i < newArray.length; i++){
+        let li = document.createElement('li');
+            li.textContent = newArray.charAt(i);
+      
+              if(li.textContent ===" "){
+                  li.className += "space";
+              } else {
+                  li.className += "letter";
+                  li.style.backgroundColor = "purple";
 
+              };
+            ul.appendChild(li);
+        };
 
+  });//end reset event listener
+}; //end playGame function
 
+//call playGame function
+playGame();
 
+                  
             
-      });//end reset event listener
-
      
+
+  
+
+
+
+    
